c57b8784ecefd48e33594151d610d9359b17bff9
Lavanya Rangaswamy <rangaswamy.lavanya@gmail.com>
2020-10-20 18:20:02 -0700

    Done

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, session, request
+from flask import Flask, render_template, session, request,redirect
 
 app = Flask(__name__)
 app.secret_key = "blahhhhhhhh"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 @app.route('/')
 def show_homepage():
@@ -16,11 +17,19 @@ def show_homepage():
 
 @app.route('/form')
 def show_form():
-    pass
+    return render_template("form.html")
 
 @app.route('/results')
 def show_results():
-    pass
+    adj = request.args.getlist('quality')
+    name = session['name']
+    return render_template("results.html",result=adj,person=name)
+
+@app.route("/save-name")
+def save_name():
+    username = request.args.get('name')
+    session['name'] = username 
+    return redirect("/form")
 
 
 if __name__ == "__main__":
